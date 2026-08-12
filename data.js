05e81b2d62be668c2d76221a6a613d626f1951c6
Spurmacher <spurmacher@bluewin.ch>
2026-08-12 12:58:39 +0200

    Datenupdate 2026-08-12_12-58 — 2 Punkte

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-﻿// Automatisch generiert von sync-map.ps1, nicht manuell bearbeiten.
+﻿// Automatisch generiert von sync-map.ps1 — nicht manuell bearbeiten.
 // Quelle: Google Sheet, synchronisiert am 2026-08-12 12:58
-const DATA = [{"id":"pt_1786532009211_ttjd95","date":"2026-08-11","category":"Accommodation","subcategory":"Guesthouse","title":"Hüsli","note":"Früh","lat":47.4238970303236,"lon":9.55776616990321,"photoUrl":"","infoUrl":"","country":""},{"id":"pt_1786532295487_76slgn","date":"2026-08-11","category":"Logistics","subcategory":"Shopping","title":"Markt","note":"","lat":47.42389182628096,"lon":9.557776735027494,"photoUrl":"","infoUrl":"","country":""}];
+var DATA = [{"id":"pt_1786532009211_ttjd95","date":"2026-08-11","category":"Accommodation","subcategory":"Guesthouse","title":"Hüsli","note":"Früh","lat":47.4238970303236,"lon":9.55776616990321,"photoUrl":"","infoUrl":"","country":""},{"id":"pt_1786532295487_76slgn","date":"2026-08-11","category":"Logistics","subcategory":"Shopping","title":"Markt","note":"","lat":47.42389182628096,"lon":9.557776735027494,"photoUrl":"","infoUrl":"","country":""}];
