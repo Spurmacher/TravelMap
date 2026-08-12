960142bc02538ac03105f4f5ec5911d5ed21152a
Spurmacher <spurmacher@bluewin.ch>
2026-08-12 13:33:15 +0200

    reset

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
 ﻿// Automatisch generiert von sync-map.ps1 — nicht manuell bearbeiten.
 // Quelle: Google Sheet, synchronisiert am 2026-08-12 13:27
-var DATA = {"id":"pt_1786533971519_shjrd4","date":"2026-08-11","category":"Accommodation","subcategory":"Guesthouse","title":"Hüsli","note":"Bereits um 5:30 aufgestanden","lat":47.42389615579166,"lon":9.557767793060203,"photoUrl":"","infoUrl":"","country":"Switzerland"};
+var DATA = [{"id":"pt_1786533971519_shjrd4","date":"2026-08-11","category":"Accommodation","subcategory":"Guesthouse","title":"Hüsli","note":"Bereits um 5:30 aufgestanden","lat":47.42389615579166,"lon":9.557767793060203,"photoUrl":"","infoUrl":"","country":"Switzerland"};]
