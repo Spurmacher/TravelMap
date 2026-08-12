2f0ceb49d5fbe9e992e2c6731801910eaa17218a
Spurmacher <spurmacher@bluewin.ch>
2026-08-12 12:55:31 +0200

    Datenupdate 2026-08-12_12-55 1 Punkte

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-﻿// Automatisch generiert von sync-map.ps1 — nicht manuell bearbeiten.
-// Quelle: Google Sheet, synchronisiert am 2026-08-12 12:54
-var DATA = {"id":"pt_1786532009211_ttjd95","date":"2026-08-11","category":"Accommodation","subcategory":"Guesthouse","title":"Hüsli","note":"Früh","lat":47.4238970303236,"lon":9.55776616990321,"photoUrl":"","infoUrl":"","country":""};
+﻿// Automatisch generiert von sync-map.ps1, nicht manuell bearbeiten.
+// Quelle: Google Sheet, synchronisiert am 2026-08-12 12:55
+const DATA = {"id":"pt_1786532009211_ttjd95","date":"2026-08-11","category":"Accommodation","subcategory":"Guesthouse","title":"Hüsli","note":"Früh","lat":47.4238970303236,"lon":9.55776616990321,"photoUrl":"","infoUrl":"","country":""};
